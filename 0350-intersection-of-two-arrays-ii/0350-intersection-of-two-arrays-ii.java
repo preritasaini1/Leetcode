72207681556477763862dcd71212e930e1a104c0
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-03-15 23:46:09 +0530

    Time: 12 ms (5.17%), Space: 43.4 MB (46.31%) - LeetHub

---
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] intersect(int[] n1, int[] n2) {
+    ArrayList<Integer> arr1=new ArrayList<>();
+	ArrayList<Integer> arr2=new ArrayList<>();
+	for(int n: n1) {
+		arr1.add(n);
+	}
+	for(int n: n2) {
+		if(arr1.contains(n)) {
+			arr2.add(n);
+            arr1.remove((Integer)n);
+		}
+	}
+	int res[]=new int[arr2.size()];
+	int i=0;
+	for(int n: arr2) {
+		res[i++]=n;
+	}
+	return res;
+    }
+}
