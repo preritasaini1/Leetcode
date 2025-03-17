0b8682da912fb56e86752e8fc91fd8dd83bc4fe8
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-03-18 00:23:47 +0530

    Time: 1 ms (63.58%), Space: 41 MB (38.88%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isHappy(int n) {
-        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> seen = new HashSet<>();  //use for breaking loop like 4 comes againg then again process and continues as it is unhappy number.
         while(n != 1 && !seen.contains(n)){
             seen.add(n);
             int sum=0;
