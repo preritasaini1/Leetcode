947694190d224421f28b7c77400a69ceb71dd6e2
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-03-21 20:09:50 +0530

    Time: 3 ms (11.12%), Space: 44.8 MB (13.35%) - LeetHub

--- 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.java
+++ 2654-count-the-number-of-vowel-strings-in-range/2654-count-the-number-of-vowel-strings-in-range.java
@@ -1,13 +1,13 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
-    int count=0;
+    /*int count=0;
 	for(int i=left;i<=right;i++){
 		if((words[i].startsWith("a") || words[i].startsWith("e")  || words[i].startsWith("i") || words[i].startsWith("o") || words[i].startsWith("u")) && ( words[i].endsWith("a") || words[i].endsWith("e") || words[i].endsWith("i") || words[i].endsWith("o") || words[i].endsWith("u")) ) {
 			count++;
 		}
 
 	}
-    return count;
+    return count;*/
 
     // Another way code-
     /*int count=0;
@@ -19,5 +19,18 @@ class Solution {
 		}
 	}
     return count;*/
+
+    //2.
+    Set<Character> vowels=Set.of('a','e','i','o','u');
+    int count=0;
+    for(int i=left;i<=right;i++){
+        String w=words[i].toLowerCase();
+        char first=w.charAt(0);
+        char last=w.charAt(w.length()-1);
+        if(vowels.contains(first) && vowels.contains(last)){
+            count++;
+        }
+    }
+    return count;
     }
 }
