706828e5dd8f6d3cd3dff06caea43205c97c5e37
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-03-24 21:46:27 +0530

    Time: 2 ms (92.7%), Space: 47.4 MB (25.97%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -6,7 +6,7 @@ class Solution {
         while(l<r){
             int sum=nums[l]+nums[r];
             if(sum==target){
-                return new int[] {l + 1, r + 1};
+                return new int[] {l + 1, r + 1};  //for reducing time limit exceeded we write like this... and l+1 , r+1 as in leetcode somtimes index start from 1!!
             }
             else if(sum>target){
                 r--;
