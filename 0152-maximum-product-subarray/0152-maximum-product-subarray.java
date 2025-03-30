12d6779f3f2e2d90d41c80236878b17f259c64e7
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-03-30 23:40:14 +0530

    Time: 2 ms (58.17%), Space: 45.3 MB (21.15%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max_product=Integer.MIN_VALUE;
+       /* int max_product=Integer.MIN_VALUE;
         for (int i=0;i<nums.length;i++){
              int product=1;
             for (int j=i;j<nums.length;j++){
@@ -8,6 +8,27 @@ class Solution {
                 max_product=Math.max(max_product,product);
             }
         }
-        return max_product;
+        return max_product;*/
+
+        //Optimmal Approach- Kadane's Algorithm
+        int max_product = nums[0];
+    int min_product = nums[0]; // Track min in case of negatives
+    int result = nums[0];
+
+    for (int i = 1; i < nums.length; i++) {
+        if (nums[i] < 0) {
+            // Swap max and min when encountering a negative number
+            int temp = max_product;
+            max_product = min_product;
+            min_product = temp;
+        }
+
+        max_product = Math.max(nums[i], max_product * nums[i]);
+        min_product = Math.min(nums[i], min_product * nums[i]);
+
+        result = Math.max(result, max_product);
+    }
+
+    return result;
     }
 }
