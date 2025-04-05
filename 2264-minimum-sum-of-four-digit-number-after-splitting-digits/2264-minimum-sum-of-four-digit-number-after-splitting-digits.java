f520a17ab0ef862424a127a2f5bc097cc93e8a3d
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-04-05 23:16:12 +0530

    Time: 0 ms (100%), Space: 40.5 MB (51.15%) - LeetHub

--- 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.java
+++ 2264-minimum-sum-of-four-digit-number-after-splitting-digits/2264-minimum-sum-of-four-digit-number-after-splitting-digits.java
@@ -6,8 +6,8 @@ class Solution {
             num /= 10;
         }
         Arrays.sort(digits);
-        int new1 = digits[0] * 10 + digits[2];
-        int new2 = digits[1] * 10 + digits[3];
+        int new1 = digits[0] * 10 + digits[2];  
+        int new2 = digits[1] * 10 + digits[3];  //as after sorting array we get smallest no. in 0th and 1st position and from that one we get minimum sum - 23+29=52;
         return new1 + new2;
     }
 }
