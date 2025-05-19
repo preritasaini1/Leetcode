ce411029b8aa3b00402b7d12e8c84b9a4f1ab1a2
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-05-19 22:28:56 +0530

    Time: 2 ms (98.53%), Space: 42.5 MB (9.52%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-       /* HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<arr.length;i++){
             if(map.containsKey(arr[i])){
                 map.put(arr[i],map.get(arr[i])+1);
@@ -13,16 +13,6 @@ class Solution {
         for(int ele:map.values()){
             set.add(ele);
         }
-        return set.size()==map.values().size(); */
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int num : arr){
-            map.put(num , map.getOrDefault(num , 0) + 1);
-        }
-        HashSet<Integer> set = new HashSet<>();
-        for(int i : map.values()){
-            set.add(i);
-        }
-        return set.size() == map.values().size();
+        return set.size()==map.values().size(); 
     }
 }
