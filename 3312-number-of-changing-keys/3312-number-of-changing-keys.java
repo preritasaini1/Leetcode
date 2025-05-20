5c99177901add72cdbce44d56488a16426edbfa8
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-05-20 20:06:32 +0530

    Time: 1 ms (99.88%), Space: 42.4 MB (40.3%) - LeetHub

--- 3312-number-of-changing-keys/3312-number-of-changing-keys.java
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countKeyChanges(String s) {
-        int d=0;
+        /*int d=0;
         int count=0;
         for(int i=1;i<s.length();i++){
             char prev=s.charAt(i-1);
@@ -10,6 +10,18 @@ class Solution {
                count++;
             }
         }
-        return count;
+        return count;*/
+
+        s= s.toLowerCase();
+        int cnt= 0;
+        char temp=s.charAt(0);
+        for(int i=1; i<s.length(); i++){
+            if(s.charAt(i)!= temp){
+                cnt++;
+                temp=s.charAt(i);
+            }
+        }
+
+        return cnt;
     }
 }
