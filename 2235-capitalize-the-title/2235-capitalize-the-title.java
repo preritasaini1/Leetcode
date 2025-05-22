edebd1ac05e4579ed9b7056c0284b2bbfec1d58d
Prerita Saini <149650718+preritasaini1@users.noreply.github.com>
2025-05-22 17:05:59 +0530

    Time: 3 ms (81.88%), Space: 42.4 MB (81.54%) - LeetHub

--- 2235-capitalize-the-title/2235-capitalize-the-title.java
+++ 2235-capitalize-the-title/2235-capitalize-the-title.java
@@ -1,6 +1,6 @@
 class Solution {
     public String capitalizeTitle(String title) {
-        String arr[]=title.split("\\s+");
+       /* String arr[]=title.split("\\s+");
         for(int i=0;i<arr.length;i++){
     	arr[i]=arr[i].toLowerCase();
     	if(arr[i].length()>2) {
@@ -10,6 +10,14 @@ class Solution {
     	}
     }
     String res=String.join(" ", arr);
-    return res;
+    return res;*/
+
+    //Optimized way-
+    StringBuilder sb = new StringBuilder();
+    for(String s : title.toLowerCase().split(" ")){
+            if(s.length() > 2) sb.append(s.substring(0, 1).toUpperCase()).append(s.substring(1)).append(" ");
+            else sb.append(s).append(" ");
+        }
+        return sb.toString().trim();
     }
 }
